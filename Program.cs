7750e347cd2b1b0513e70192db95f0e06ac076cb
serioclaudio <48640348+serioclaudio@users.noreply.github.com>
2019-09-28 21:07:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace FileCopyUtility_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoTest1(args);
+        }
+
+        static void DoTest1(string[] args)
+        {
+            if (args.Length == 0 )
+            {
+                Console.WriteLine("  Error:");
+                Console.WriteLine("    Process File must be specified as a command-line parameter.");
+                Console.WriteLine("    Correct usage: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Name + " <ProcessFile.txt>");
+                Console.WriteLine("      where <ProcessFile.txt> is the file containing the configuration tags for the application.");
+                return;
+            }
+
+            cFileProcess cfp = new cFileProcess(args[0]);
+
+            return;
+        }
+    }
+}
